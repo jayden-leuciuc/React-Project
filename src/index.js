46930457ecbe2fa20c8d3cfd8338f85545f0d336
Jayden Leuciuc <leuciucjayden@gmail.com>
2020-11-17 11:17:02 -0500

    Prevent heroku crash

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './sass/main.scss';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
